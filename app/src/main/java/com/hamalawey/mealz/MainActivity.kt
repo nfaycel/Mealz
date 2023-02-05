68498e12c6106f2d0b2f453f384ffe60790c3640
nfaycel <debian2007.faical@gmail.com>
2023-02-05 21:15:33 +0100

    10. Run the App

--- app/src/main/java/com/hamalawey/mealz/MainActivity.kt
+++ app/src/main/java/com/hamalawey/mealz/MainActivity.kt
@@ -2,10 +2,30 @@ package com.hamalawey.mealz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MealsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv: RecyclerView = findViewById(R.id.category_rv)
+
+        viewModel.getMeals()
+        val mealsAdapter = MealsAdapter()
+
+        lifecycleScope.launch {
+            viewModel.categories.collect {
+                mealsAdapter.submitList(it?.categories)
+                rv.adapter = mealsAdapter
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hamalawey/mealz/MealzApplication.kt
@@ -0,0 +1,8 @@
+package com.hamalawey.mealz
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MealzApplication : Application() {
+}
